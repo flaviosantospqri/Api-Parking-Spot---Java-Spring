dcf4fcc0a5af76185e334fb4f73b0661b13053eb
Flavio Santos <flavio.pasantos@outlook.com>
2022-10-05 07:44:18 -0300

    stc: add configuration bd

---
+++ src/main/java/com/api/parking/control/repositories/ParkingSpotRepository.java
@@ -0,0 +1,12 @@
+package com.api.parking.control.repositories;
+
+import com.api.parking.control.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+}
---
+++ src/main/java/com/api/parking/control/services/ParkingSpotService.java
@@ -0,0 +1,15 @@
+package com.api.parking.control.services;
+
+import com.api.parking.control.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+    @Autowired
+   final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+}
